dcc55985a19d05956f61c12f42db29abb91bdefb
Quentin Dejean <quentin.dejean34@gmail.com>
2014-12-10 16:03:09 +1100

    Added some more stuff

--- ngParcelService.js
+++ ngParcelService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ngFrench', [])
+angular.module('ParcelService', [])
     .service('ParcelService', function () {
         this.saySomethingFrench = function () {
             console.log('believe really awesome?!!!! Et re-voila! Oops! lol!');
