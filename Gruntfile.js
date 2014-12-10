fb45c983fd924541e9e3eded2ccb5a554c89fdb8
Quentin Dejean <quentin.dejean34@gmail.com>
2014-12-10 16:09:19 +1100

    new stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         bump: {
-            options: {
-                files: ['package.json'],
-                updateConfigs: [],
-                commit: true,
-                commitMessage: 'Release v%VERSION%',
-                commitFiles: ['package.json'],
-                createTag: true,
-                tagName: 'v%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: true,
-                pushTo: 'upstream',
-                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-                globalReplace: false
+            scripts: {
+                files: ["ngParcelService.js"],
+                updateConfigs: ["pkg"],
+                commitFiles: ["-a"],
+                push: false
             }
         }
     });
--- ngParcelService.js
+++ ngParcelService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ParcelService', [])
+angular.module('ngParcelService', [])
     .service('ParcelService', function () {
         this.saySomethingFrench = function () {
             console.log('believe really awesome?!!!! Et re-voila! Oops! lol!');
