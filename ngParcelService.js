035ead66bf9a73b38f1105676167045a7d1b5d25
Quentin Dejean <quentin.dejean34@gmail.com>
2014-12-10 15:02:34 +1100

    first push

---
+++ ngParcelService.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('ngFrench', [])
+    .service('ParcelService', function () {
+        this.saySomethingFrench = function () {
+            console.log('Cannot believe it awesome?!!!! Et re-voila! Oops! lol!');
+        };
+    });
